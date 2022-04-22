f499be6a21e330a05367a34251109ba016d68b6b
Ujjal Sharma <ujjalsharma81@gmail.com>
2022-04-22 22:28:42 +0530

    added video analyze cnn

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import tensorflow_addons as tfa
 from tensorflow import keras
 import uuid
 import urllib
+import cv2
 
 from flask import Flask, request, abort, jsonify, send_from_directory
 
@@ -24,9 +25,12 @@ api = Flask(__name__,  static_url_path='', static_folder='static')
 
 def download_video_url(req_data):
     video_name = str(uuid.uuid1()) + ".mp4"
-    ret = req_data["video_url"]
-    ret = "youtube-dl -o './video_uploads/"+video_name+" "+ ret
-    os.system(ret)
+    video_url = req_data["video_url"]
+    video_path = "./video_uploads/"+video_name
+    # ret = "youtube-dl -o './video_uploads/"+video_name+" "+ ret
+    # os.system(ret)
+    
+    urllib.request.urlretrieve(video_url, video_path)
     return video_name
 
 def get_video_name(video_url):
@@ -44,7 +48,7 @@ def hello_world():
 
 
 @api.route("/analyze_video", methods=["POST"])
-def post_file():
+def analyze_video_json():
 
     # download the video from the url
     req_data = request.get_json()
@@ -70,26 +74,6 @@ def post_file():
 
     return jsonify(ret_pred)
 
-@api.route("/generate_sei", methods=["POST"])
-def sei_generation():
-
-    # download the video from the url
-    req_data = request.get_json()
-    
-    video_name = download_video_url(req_data)
-    video_path = "./video_uploads/"+video_name
-    # run openpose on the video and save skeletons on skeletons/videoname folder
-
-    # compute the sei and save it in static/sei folder
-
-    sei_file_name = "" #filename for the saved SEIs goes here
-    # return the URL for the 
-    sei_url = "/static/sei/"+sei_file_name
-    ret = {
-        "sei_url": sei_url
-    }
-
-    return jsonify(ret)
 
 def download_image_url(req_data):
     image_url = req_data['image_url']
@@ -98,17 +82,39 @@ def download_image_url(req_data):
     urllib.request.urlretrieve(image_url, image_path)
     return image_name
 
-@api.route("/predict_SEI", methods=["POST"])
-def sei_prediction():
+def convert_to_black_and_white(img):
+    I = img.shape[0]
+    J = img.shape[1]
+    new_img = np.zeros((I,J))
+    for i in range(I):
+        for j in range(J):
+            if (img[i][j]!=0).any():
+                new_img[i][j]=255
+    return new_img
 
-    # get url and image
-    req_data = request.get_json()
-    image_name = download_image_url(req_data)
+def convert_to_sei(skeletons_folder_path, sei_folder_path):
+    list = os.listdir(skeletons_folder_path) # dir is your directory path
+    number_files = len(list)
+    
+    img = cv2.imread(skeletons_folder_path+"/"+list[0])
+    g_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    sei_img = np.zeros(g_img.shape)
+    
+    for image_file_name in list:
+        n_img = cv2.imread(skeletons_folder_path+"/"+image_file_name)
+        #n_g_img = cv2.cvtColor(n_img, cv2.COLOR_BGR2GRAY)
+        n_g_img = convert_to_black_and_white(n_img)
+        sei_img  = sei_img+n_g_img
+        
+    sei_img = sei_img/number_files;
+    cv2.imwrite(sei_folder_path, sei_img)
 
-    image_path = "./image_uploads/"+image_name
-
+def predict_sei(image_path):
     # open image and preprocess
-    input_image = np.array(Image.open(image_path))
+    im = Image.open(image_path)
+    im = im.resize((224,224))
+    input_image = np.array(im)
     input_image = input_image.reshape((1, 224,224, 1))
     input_image = input_image/255.0
     cnn_output = cnn_model.predict(input_image)
@@ -125,6 +131,40 @@ def sei_prediction():
 
 
     return jsonify(ret_dic)
+
+@api.route("/analyze_video_CNN", methods=["POST"])
+def cnn_video_prediction():
+    # download the video from the url
+    req_data = request.get_json()
+    
+    video_name = download_video_url(req_data)
+    video_path = "../video_uploads/"+video_name
+    skeleton_folder_path = "../static/saved_skeletons/"+video_name[:-4]+"/"
+    os.mkdir("static/saved_skeletons/"+video_name[:-4])
+    # run openpose on the video path
+    commnd = "!(cd openpose/ && bin/OpenPoseDemo.exe --video "+video_path+" --net_resolution 160x160 --write_images "+skeleton_folder_path+" --disable_blending)"
+    os.system(commnd)
+
+    # filter only gait cycle and process the images
+
+    # saved the seis
+    sei_path = "./static/seis/"+video_name[:-4]+".jpg"
+    skeleton_folder_path = "static/saved_skeletons/"+video_name[:-4]+"/"
+    convert_to_sei(skeleton_folder_path, sei_path)
+
+    return predict_sei(sei_path)
+
+
+@api.route("/predict_SEI", methods=["POST"])
+def sei_prediction():
+
+    # get url and image
+    req_data = request.get_json()
+    image_name = download_image_url(req_data)
+
+    image_path = "./image_uploads/"+image_name
+
+    return predict_sei(image_path)    
 
 if __name__ == "__main__":
     api.run(debug=True, host='0.0.0.0', port=8000)
